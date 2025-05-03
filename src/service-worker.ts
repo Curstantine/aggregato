1af1434121595d88d0d8fc71fa9b7e6a93269c8b
Curstantine <curstantine@proton.me>
2025-05-03 14:21:24 +0530

    chore: update packages, and migrate the param creator to url creator

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -5,7 +5,7 @@
 /// <reference types="../.svelte-kit/ambient.d.ts" />
 
 import { wait } from "@jabascript/core";
-import { createSearchParams } from "@jabascript/core/query";
+import { createURL } from "@jabascript/query";
 
 import { PUBLIC_LASTFM_API_KEY } from "$env/static/public";
 
@@ -41,15 +41,15 @@ sw.addEventListener("message", (event) => {
 });
 
 async function importLastFm(username: string, mode: ImportModeType) {
-	const params = createSearchParams({
+	const url = createURL("https://ws.audioscrobbler.com/2.0", {
 		method: "user.gettopartists",
 		format: "json",
 		user: username,
 		api_key: PUBLIC_LASTFM_API_KEY,
-		limit: { "top-10": "10", "top-50": "50", all: "100" }[mode]
+		limit: [{ "top-10": "10", "top-50": "50", all: "100" }, mode]
 	});
 
-	const resp = await fetch("https://ws.audioscrobbler.com/2.0?" + params.toString());
+	const resp = await fetch(url);
 	const body = (await resp.json()) as LastfmTopArtistData;
 
 	if (!resp.ok) {
